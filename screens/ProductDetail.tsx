372e052a410e70cb7cb1afdeeb87867c394a9fb9
Alyss-Aelle <alyssferjul@gmail.com>
2023-09-27 12:59:36 -0400

    new

---
+++ screens/ProductDetail.tsx
@@ -0,0 +1,29 @@
+// import {
+//   Image,
+//   SafeAreaView,
+//   ScrollView,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+// import React, { useState } from "react";
+// import { NativeStackScreenProps } from "@react-navigation/native-stack";
+// import { RootStackParamList } from "../types";
+// import { Ionicons } from "@expo/vector-icons";
+// import Spacing from "../constants/Spacing";
+// import Colors from "../constants/Colors";
+// import Font from "../constants/Font";
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProductDetail = () => {
+  return (
+    <View>
+      <Text>ProductDetail</Text>
+    </View>
+  )
+}
+
+export default ProductDetail
